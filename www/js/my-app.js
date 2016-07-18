81a935bd391bc52063e4e734ce0a4b0f69fe7048
Barak2222 <barakcohen54@gmail.com>
2016-07-18 13:16:06 +0300

    f

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -13,13 +13,82 @@ var mainView = myApp.addView('.view-main', {
 });
 
 
-
-var createCommentCard = function(author, message){
-	var st = '<div class="card" id="aaa"><div class="card-content"><div class="card-content-inner">'
+var comments = {
+    createCommentCard: function(author, message){
+        var st = '<div class="card" id="aaa"><div class="card-content"><div class="card-content-inner">'
             +  '<b>' + author + ':</b> ' + message + '</div></div></div>';
-    return $(st);
-}
+        return $(st);
+    }
+};
+
 $(document).ready(function(){
-	var card = createCommentCard('Sapir', "Hii!!!");
+	var card = comments.createCommentCard('Sapir', "Hii!!!");
 	$('#commentsSec').append(card);
 });
+
+
+
+
+
+/** EXAMPLES:
+
+    logout: function(){
+        $.getJSON( "/www/logout", function( data ) {
+            console.log(data);
+            if(data == true || data == "true"){
+                window.location.href = "/public/login.html";
+            } else {
+                console.log('error disconnecting');
+                console.log(data);
+            }
+        })
+        .fail(function(){
+            console.log('error while trying to get data from server');
+        })
+    }
+
+GETQUOTE:
+
+var randomQuote = {
+    init: function(){
+        $.getJSON( "/www/getQuote", function( data ) {
+            $('#randomQuote').text(data);
+        })
+        .fail(function(){
+            console.log('error while trying to get data from server');
+        })
+    }
+}
+
+    setValToServer: function(val){
+        $.post("/www/setCalc/" + val)
+        .fail(function(data){
+            console.log('failed to post to server! ' + data);
+        });
+    },
+
+
+SUBMIT FORM:
+
+$('form').on('submit', function(e){
+    e.preventDefault();
+    var form = $(this);
+    var userData = form.serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        data: userData,
+    }).done(function(dataRecieved){
+        if(dataRecieved){
+            window.location.href = "/www/index.html";
+        } else {
+            window.location.href = "/public/accessDenied.html";
+        }
+        form.trigger('reset');
+    })
+    .fail(function(data){
+        window.location.href = "/public/accessDenied.html";
+    });
+});
+    */
