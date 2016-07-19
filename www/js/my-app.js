cd65792a001660a564ce6a215bef1ce96923de5a
Barak2222 <barakcohen54@gmail.com>
2016-07-19 23:53:58 +0300

    client side bug fixes and minor improvements

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,4 @@
 var globalData;
-// Initialize your app
 var myApp = new Framework7({
     animateNavBackIcon:true
 });
@@ -13,24 +12,41 @@ var mainView = myApp.addView('.view-main', {
     domCache: true
 });
 
+utils = {
+    dateFormatter(d){
+        return d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+    },
+    isSameDay: function(d1, d2){
+        d1 = new Date(d1);
+        d2 = new Date(d2);
+        if(d1.getDate() != d2.getDate()){ return false; }
+        if(d1.getFullYear() != d2.getFullYear()){ return false; }
+        if(d1.getMonth() != d2.getMonth()){ return false; }
+        return true;
+    },
+}
+
+
 var navigation = {
     goBack: function(){
         var l = document.getElementById('form-go-back');
-        l.click();
+        for(var i = 0; i < 50; i++){
+            l.click();            
+        }
     },
     toIDCTab: function(){
-        var l = document.getElementById("tabToIDC");
-        l.click();
+        myApp.showTab('#tab1');
     },
     fromIDCTab: function(){
-        var l = document.getElementById("tabFromIDC");
-        l.click();
+        myApp.showTab('#tab2');
     },
 }
 
 
 
 $(document).ready(function(){
+    $('#logoutB').on('click', logout);
+    $("#commentForm").on('submit', comments.createCommentHandler);
     var card = comments.createCommentCard('Sapir', "Hii!!!");
     $('#commentsSec').append(card);
     var futureData;
@@ -57,14 +73,7 @@ newPostSubmit = {
                 data: userData,
             }).done(function(dataRecieved){
                 if(dataRecieved){
-                    //Close new post screen
                     navigation.goBack();
-                    
-                    /**setTimeout(function(){
-                        // switch tab
-                        (dataRecieved.type == "toIDC") ? navigation.toIDCTab() : navigation.fromIDCTab();
-                    }, 1000);*/    //doesnt work currently
-
                     rides.addOnePost(dataRecieved);
                     form.trigger('reset');    
                 } else {
@@ -101,9 +110,9 @@ var rides = {
         var today = new Date();
         var tomorrow = new Date();
         tomorrow.setDate(tomorrow.getDate() + 1);
-        if(this.isSameDay(obj.date, today)){
+        if(utils.isSameDay(obj.date, today)){
             st+= "today";
-        } else if(this.isSameDay(obj.date, tomorrow)){
+        } else if(utils.isSameDay(obj.date, tomorrow)){
             st+= "tomorrow";
         } else {
             st+= "later";
@@ -111,22 +120,13 @@ var rides = {
         st+= (obj.type == "toIDC") ? "ToIDC" : "FromIDC";
         return st;
     },
-    isSameDay: function(d1, d2){
-        d1 = new Date(d1);
-        d2 = new Date(d2);
-        if(d1.getDate() != d2.getDate()){ return false; }
-        if(d1.getFullYear() != d2.getFullYear()){ return false; }
-        if(d1.getMonth() != d2.getMonth()){ return false; }
-        return true;
-    },
     createNode: function(obj){
         var time = obj.hour[0] + ':' + obj.hour[1];
         var $item = $('<div class="item-title">' + obj.from + " to " + obj.to + '</div>');
         var $row = $('<div class="item-title-row"></div>');
         $row.append($item);
-
-        // TODO: leaving OR requesting!!
-        var $span = $('<span class="timeSpan">Leaving ' + time + '</span>');
+        var leavingOrRequesting = (obj.role == "driver") ? "Leaving " : "Requesting ";
+        var $span = $('<span class="timeSpan">' + leavingOrRequesting + time + '</span>');
         var $subtitle = $('<div class="item-subtitle">' + obj.author + '</div>');
         $subtitle.append($span);
         $inner = $('<div class="item-inner"></div>').append($row).append($subtitle);
@@ -157,131 +157,65 @@ var ridePage = {
         })
     },
     createPage: function(data){
-        console.log(data);
         $("#ridePageAuthor").text(data.author);
         var role = (data.role == "driver") ? "driver" : "pedestrian";
         document.getElementById('ridePageImg').src = "img/" + role + ".png";
         $("#ridePageFromTo").text("From " + data.from + " to " + data.to);
-        $("#ridePageTime").text("Time: " + data.hour);//TODO: change to have both hour and date in valid format
+        var time = data.hour[0] + ":" + data.hour[1] + " - " + utils.dateFormatter(new Date(data.date));
+        $("#ridePageTime").text("Time: " + time);
         $("#ridePageNotes").text(data.notes);
         comments.init(data.comments);
     },
 }
-/**
-ridePageAuthor
-ridePageImg
-ridePageFromTo   // From Tel aviv to IDC.
-ridePageTime    //Time: 20:30
-ridePageSpots    // 3 spots
 
 
-
-*/
-
-
-
-function Highlighter($node){
-    var i = 1;
-    var colors = ["#ffff99", "#ffffb3", "#ffffcc", "#ffffe6", "#ffffff"];
-    $node.css({"background-color": colors[0]});
-    
-    var tFunc = function() {
-        setTimeout(function(){
-            $node.css({"background-color": colors[i]});
-            i++;
-            if(i < colors.length){
-                tFunc();
+var comments = {
+    createCommentHandler: function(e){
+        e.preventDefault();
+        comments.addOneComment();
+    },
+    highlightCallback: function($node){
+        new Highlighter($node);
+    },
+    addOneComment: function(){
+        var message = document.getElementById('message').value;
+        $.post('/www/CreateNewComment', {message: message , rideID: ridePage.current} , function(data){
+            if(data){
+                comments.createCommentComponent(data, comments.highlightCallback);
+                document.getElementById('message').value = "";
             }
-        }, 250);
-    }
-    setTimeout(function(){
-        tFunc();
-    }, 500);
-}
-
-
-
-/**
-                <div id="profileSec" class="card">
-                  <div class="card-header">Barak Cohen<div class="item-media"><img src="img/driver.png" width="44"></div></div>
-                  <div class="card-content">
-                    <div class="card-content-inner">
-                      <p>From Tel aviv to IDC.</p>
-                      <p>Time: 20:30</p>
-                    </div>
-                  </div>
-                  <div class="card-footer">3 spots</div>
-                </div>
-
-*/
-/** EXAMPLES:
-
-    logout: function(){
-        $.getJSON( "/www/logout", function( data ) {
-            console.log(data);
-            if(data == true || data == "true"){
-                window.location.href = "/public/login.html";
-            } else {
-                console.log('error disconnecting');
-                console.log(data);
+            else{
+                alert("error uploading the comment");
             }
-        })
-        .fail(function(){
-            console.log('error while trying to get data from server');
-        })
-    }
-
-GETQUOTE:
-
-var randomQuote = {
-    init: function(){
-        $.getJSON( "/www/getQuote", function( data ) {
-            $('#randomQuote').text(data);
-        })
-        .fail(function(){
-            console.log('error while trying to get data from server');
-        })
-    }
-}
-
-    setValToServer: function(val){
-        $.post("/www/setCalc/" + val)
-        .fail(function(data){
-            console.log('failed to post to server! ' + data);
         });
     },
-
-
-SUBMIT FORM:
-
-$('form').on('submit', function(e){
-    e.preventDefault();
-    var form = $(this);
-    var userData = form.serialize();
-
-    $.ajax({
-        type: 'POST',
-        url: '/login',
-        data: userData,
-    }).done(function(dataRecieved){
-        if(dataRecieved){
-            window.location.href = "/www/index.html";
-        } else {
-            window.location.href = "/public/accessDenied.html";
+    createCommentCard: function(author, message){
+        var st = '<div class="card" id="aaa"><div class="card-content"><div class="card-content-inner">'
+            +  '<b>' + author + ':</b> ' + message + '</div></div></div>';
+        return $(st);
+    },
+    createCommentComponent: function(data, callback){
+        var name = data.author;
+        var $node = comments.createCommentCard(name, data.message);
+        $("#commentsSec").append($node);
+        
+        if(callback){
+            callback($node);
         }
-        form.trigger('reset');
-    })
-    .fail(function(data){
-        window.location.href = "/public/accessDenied.html";
-    });
-});
-    */
-$(document).ready(function(){
-    $('#logoutB').on('click', logout1);
-});
+    },
+    empty: function(){
+        $("#commentsSec").empty();
+    },
+    init: function(arr){
+        comments.empty();
+        for (var i = 0; i < arr.length; i++) {
+            this.createCommentComponent(arr[i]);
+        }
+    },
+}
 
 
-var logout1 = function(){
+var logout = function(){
     console.log('dd');
     $.getJSON( "/www/logout", function( data ) {
         if(data == true || data == "true"){
@@ -295,51 +229,21 @@ var logout1 = function(){
     })
 };
 
-
-
-
-function makeComment(){
-    comments.addOneComment();
-};
-
-
-/**
-    this.author = author;
-    this.rideID = rideID; // parent node
-    this.message = message;
-    this.time = new Date();
-*/
-var comments = {
-    addOneComment: function(){
-        var message = document.getElementById('message').value;
-        //change the id o in the rideID||| DONE test if working
-        $.post('/www/CreateNewComment', {message: message , rideID: ridePage.current} , function(data){
-            if(data){
-                comments.createCommentComponent(data);
-                document.getElementById('message').value = "";
+function Highlighter($node){
+    var i = 1;
+    var colors = ["#ffff99", "#ffffa3", "#ffffad", "#ffffb8", "#ffffc2", "#ffffcc", "#ffffd6", "#ffffe0", "#ffffeb", "#fffff5", "#ffffff"];
+    $node.css({"background-color": colors[0]});
+    
+    var tFunc = function() {
+        setTimeout(function(){
+            $node.css({"background-color": colors[i]});
+            i++;
+            if(i < colors.length){
+                tFunc();
             }
-            else{
-                alert("error uploading the comment");
-            }
-        });
-    },
-    createCommentCard: function(author, message){
-        var st = '<div class="card" id="aaa"><div class="card-content"><div class="card-content-inner">'
-            +  '<b>' + author + ':</b> ' + message + '</div></div></div>';
-        return $(st);
-    },
-    createCommentComponent: function(data){
-        var name = data.author;
-        var node = comments.createCommentCard(name, data.message);
-        $("#commentsSec").append(node);
-    },
-    empty: function(){
-        $("#commentsSec").empty();
-    },
-    init: function(arr){
-        comments.empty();
-        for (var i = 0; i < arr.length; i++) {
-            this.createCommentComponent(arr[i]);
-        }
-    },
+        }, 150);
+    }
+    setTimeout(function(){
+        tFunc();
+    }, 500);
 }
